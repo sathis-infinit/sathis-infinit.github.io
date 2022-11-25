f71fd22eae9be5474fd819a04411b126bfa56295
Sathish Kumar <s.sathish.contact@gmail.com>
2022-11-25 11:21:07 +0530

    backup

--- luna_brain/lunatask.js
+++ luna_brain/lunatask.js
@@ -1,10 +1,21 @@
 
 async function listerncmd() {
 
-        window.curcmd = await lunarecognise()
+        var curcmd = await lunarecognise();
+        if(curcmd != undefined)
+        {
+            console.log(curcmd);
+            handleallcommand(window.curcmd);
+        }
+        else
+        {
+            lunarecognise();
+            console.log(curcmd);
+        }
+        
+
     
 }
-handleallcommand(window.curcmd);
 
 
 
@@ -143,3 +154,41 @@ function currtime() {
     console.log(ctime);
     return ctime;
 }
+
+
+
+
+async function lunarecognise() {
+    try 
+    {
+  
+      console.log("...Recognising...")
+      const recognition = new webkitSpeechRecognition();
+      recognition.continuous = false;
+      recognition.interimResults = false;
+      await recognition.start();
+      recognition.onresult = function (event) {
+        let result = '';
+        for (let i = event.resultIndex; i < event.results.length; i++) 
+        {
+          result += event.results[i][0].transcript;
+        }
+  
+        // recognition.stop();
+        // console.log(result);
+        return result;
+  
+      }
+  
+      }
+      catch (error) {
+        // If any of the awaited promises was rejected, this catch block
+        // would catch the rejection reason
+        return null;
+    }
+    
+  
+  
+  };
+   
+  
